e590a1b158c71164fab2b23f7e793dff1d53d70b
Winston Wu <pfunto.wu@gmail.com>
2021-11-11 18:24:42 -0800

    Purchaser testing for addPurchaser

--- packages/web/src/components/PurchaserMenu/AddPurchaser.test.tsx
+++ packages/web/src/components/PurchaserMenu/AddPurchaser.test.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Provider } from 'react-redux';
 import AddPurchaser from './AddPurchaser';
+import store from '../../app/store';
 
-// mocking
-
-describe('Purchaser Menu', () => {
-  // let input: HTMLInputElement;
-
+describe('Purchaser', () => {
   beforeEach(() => {
-    render(<AddPurchaser />);
-
-    // input = screen.getByRole('input', { name: 'name' });
-
-    // getByTestId
-    // findBy
+    render(
+      <Provider store={store}>
+        <AddPurchaser />
+      </Provider>
+    );
   });
 
-  // beforeAll
-  // afterEach
-  // afterAll
+  test('generates a user after inputs text and clicks submit button', async () => {
+    // should show no user initially
+    expect(screen.queryByText(/edit/i)).not.toBeInTheDocument();
 
-  // userEvent.type(input, 'a');
+    // after clicking input, user should be able to type
+    userEvent.type(screen.getByRole('textbox'), 'John Doe');
+    expect(screen.getByRole('textbox')).toHaveValue('John Doe');
 
-  // expect error message
-
-  test('renders input', () => {
-    // expect(input).toBeInTheDocument();
+    // after clicking submit, it should now display a Purchaser
+    // and updates React state
+    fireEvent.click(screen.getByRole('button', { name: /Submit/i }));
+    expect(await screen.findByText(/edit/i)).toBeInTheDocument();
   });
 
-  // test('renders input', () => {
-  //   expect(input).toBeInTheDocument();
-  // });
+  // test('edits a user after inputs text and clicks edit button', async () => {
 
-  // test('renders input', () => {
-  //   expect(input).toBeInTheDocument();
   // });
 });
