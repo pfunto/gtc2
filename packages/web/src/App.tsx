b9de9bbd285b77ae12579fe842048f0669df737e
Winston Wu <pfunto.wu@gmail.com>
2021-12-22 12:45:27 -0800

    firebase hooked up to App

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -5,9 +5,11 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 // import ky from 'ky';
 // import { PurchaseState } from './app/store';
-// import StepNav from './components/ui/StepNav';
 import Purchase from './routes/purchase';
 import CalculationForm from './modules/CalculationForm';
+import { useState } from 'react';
+import firebase from './firebase';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 // async function createPurchase(purchaseState: PurchaseState, userId: string) {
 //   const purchase = await ky.post('http://localhost:8888/api/purchases', {
@@ -18,12 +20,21 @@ import CalculationForm from './modules/CalculationForm';
 // }
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   // const purchaseState = useAppSelector((state) => state);
   // const userId = '73b013c6-371b-4266-b298-0a2ebb265a85';
+
+  const auth = getAuth(firebase);
+  onAuthStateChanged(auth, (user) => {
+    return user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+  });
+
+  console.log(`isLoggedIn`, isLoggedIn);
+
   return (
     <>
       <BrowserRouter>
-        {/* <StepNav /> */}
         <Routes>
           <Route path="/">create purchase, home</Route>
           <Route path="purchases/:purchaseId" element={<Purchase />} />
