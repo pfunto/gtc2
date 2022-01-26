66a6886a3debc57801a6a4c8be323040945bf31e
Winston Wu <pfunto.wu@gmail.com>
2022-01-26 15:48:09 -0800

    added domain to constants

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,3 +1,6 @@
-export const __prod__ = process.env.NODE_ENV === 'production'
-export const COOKIE_NAME = 'qid'
-export const ALLOWED_DOMAINS = ['http://localhost:7777']
+export const __prod__ = process.env.NODE_ENV === 'production';
+export const COOKIE_NAME = 'qid';
+export const ALLOWED_DOMAINS = [
+  'http://localhost:7777',
+  'https://group-tip-calculator-frontend.herokuapp.com',
+];
